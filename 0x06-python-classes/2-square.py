f45db7c460f17dd773b5e2546d0bb1f44ec50a90
Nusaibarh <umarnusaiba965@gmail.com>
2022-12-20 12:30:27 -0800

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,8 +7,8 @@ class Square:
     Attributes:
         __size (int): size of a side of the square
     """
-        def __init__(self, size=0):
-            """initializes the square
+    def __init__(self, size=0):
+        """initializes the square
         Args:
             size (int): size of a side of the square
         Returns:

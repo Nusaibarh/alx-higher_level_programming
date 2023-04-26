ec378791a4bc727bd29416241d6f4b223f65197f
Nusaibarh <umarnusaiba965@gmail.com>
2023-04-27 00:12:17 +0100

    5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// gets the body and write to a file
+
+const request = require('request');
+const fs = require('fs');
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
